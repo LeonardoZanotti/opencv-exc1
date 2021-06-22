c5b7f3313b16480f96fc7ffc6e7c00cc254a83ef
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-06-21 22:24:03 -0300

    Exercise 8

--- exercise-8.py
+++ exercise-8.py
@@ -2,23 +2,23 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-img = cv2.imread('choc.png')
+img = cv2.imread('flor.jpg')
 
 img_orig = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, bin_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-blur_img = cv2.blur(bin_img, (5, 5))
-can_img = cv2.Canny(bin_img, 586, 388)
+gau_img = cv2.GaussianBlur(gray, (5, 5), 0)
+ret1, gau_bin_img = cv2.threshold(gau_img, 127, 255, cv2.THRESH_BINARY)
+ret2, gray_bin_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-plt.subplot(321),plt.imshow(img_orig),plt.title('Original')
+plt.subplot(321),plt.imshow(gray, cmap="gray"),plt.title('Gray')
 plt.xticks([]), plt.yticks([])
-plt.subplot(322),plt.imshow(gray, cmap="gray"),plt.title('Gray')
+plt.subplot(322),plt.imshow(gau_img, cmap="gray"),plt.title('Gaussian Filter')
 plt.xticks([]), plt.yticks([])
-plt.subplot(323),plt.imshow(bin_img, cmap="gray"),plt.title('Binary')
+plt.subplot(323),plt.imshow(gray_bin_img, cmap="gray"),plt.title('Gray tresholded')
 plt.xticks([]), plt.yticks([])
-plt.subplot(324),plt.imshow(blur_img, cmap="gray"),plt.title('Blur filter')
+plt.subplot(324),plt.imshow(gau_bin_img, cmap="gray"),plt.title('Gaussian filter tresholded')
 plt.xticks([]), plt.yticks([])
-plt.subplot(325),plt.imshow(can_img, cmap="gray"),plt.title('Canny filter')
+plt.subplot(325),plt.imshow(img_orig),plt.title('Original')
 plt.xticks([]), plt.yticks([])
 plt.show()
